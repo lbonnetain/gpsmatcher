a50e86f2d90cbd43d6a17f44799f3d500317b116
lbonnetain <loic.bonnetain@entpe.fr>
2024-02-28 10:44:45 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import re
 import os
 
 VERSION = '0.1.0'
-here = path.abspath(path.dirname(__file__))
+here = os.path.abspath(os.path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
